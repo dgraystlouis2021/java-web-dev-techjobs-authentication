ac1da6b14e53f0272f469dcb99a9e7a4c5138b48
unknown <dgray.stlouis@gmail.com>
2022-07-03 23:04:46 -0500

    initial commit

---
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/models/User.java
@@ -0,0 +1,30 @@
+package org.launchcode.javawebdevtechjobsauthentication.models;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class User extends AbstractEntity{
+    @NotNull
+    private String username;
+    @NotNull
+    private String passwordHash;
+    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    public User(){}
+
+    public User(String username, String password) {
+        this.username = username;
+        this.passwordHash = encoder.encode(password);
+    }
+
+    public boolean isMatchingPassword(String password) {
+        return encoder.matches(password, passwordHash);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
